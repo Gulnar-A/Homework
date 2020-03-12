08e530ca11228276c0eaaee72c60d5eb1a4ff028
gulnar8 <gulnar.agayeva@gmail.com>
2020-03-11 22:34:15 -0400

    My 1st Homework

--- src/main/java/com/cbt/tests/NavigationTests.java
+++ src/main/java/com/cbt/tests/NavigationTests.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class NavigationTests {
-
+//
    // static WebDriver driver;
     public static void main(String[] args) {
 testChrome();
